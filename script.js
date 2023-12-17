92bd433357371f9461abeb47d58c63a5330c1a8b
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-17 17:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	let arr = [];
+	if(str === null) return arr;
+	else{
+		i=0;
+		while (i<str.length) {
+			let s = str.slice(i,size+i);
+			i=i+size;
+			arr.push(s);
+		}
+		return arr;
+	}
 }
 
 // Do not change the code below
